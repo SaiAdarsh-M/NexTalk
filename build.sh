de3ca2556ec102aad7f6d0f58053c6d2c94db5e7
SaiAdarsh-M <saiadarsh.work@gmail.com>
2026-04-21 22:18:50 +0530

    added asio dependency

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Checking crow files..."
-ls Crow/include/crow
+echo "Installing dependencies..."
+apt-get update
+apt-get install -y libasio-dev
 
+echo "Building project..."
 g++ server.cpp src/utils/Database.cpp -o server -std=c++17 -lpthread -I./Crow/include
