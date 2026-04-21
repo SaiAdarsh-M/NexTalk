4d7bbec78eaf5499cc4e0a4617d2f1da87388875
SaiAdarsh-M <saiadarsh.work@gmail.com>
2026-04-21 20:48:31 +0530

    removed duplicate crow clone

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# download crow
-git clone https://github.com/CrowCpp/Crow.git
-
-# compile
-g++ server.cpp src/utils/Database.cpp -o server -std=c++17 -lpthread -ICrow/include
+g++ server.cpp src/utils/Database.cpp -o server -std=c++17 -lpthread -I./Crow/include
